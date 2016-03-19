7a8875d2965e4c64c69f966f1f8281a43988c642
DavidGayo <crowin@hotmail.com>
2016-03-18 19:10:44 -0600

    restricciones 18-03-16

--- src/Crm/CaracteristicasBundle/Form/MemoriaRamType.php
+++ src/Crm/CaracteristicasBundle/Form/MemoriaRamType.php
@@ -17,7 +17,7 @@ class MemoriaRamType extends AbstractType
         $builder
             ->add('marca','text',array('attr' => array('class' => 'form-control')))
             ->add('modelo','text',array('attr' => array('class' => 'form-control')))
-            ->add('capacidad','integer',array('attr' => array('class' => 'form-control')))
+            ->add('capacidad','integer',array('attr' => array('class' => 'form-control','min'=>'1','max'=>'32')))
             ->add('unidad','choice',array('choices' => array('GigaByte' => 'GB'),'attr' => array('class' => 'form-control')))
             ->add('tipo','text',array('attr' => array('class' => 'form-control')))
             ->add('terminoMemoria','text',array('attr' => array('class' => 'form-control')))
