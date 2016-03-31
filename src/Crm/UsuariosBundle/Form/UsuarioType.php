820f42483d7f83bc9246138a860c226b42dd8154
DavidGayo <crowin@hotmail.com>
2016-03-30 19:05:38 -0600

    type de editar usuario 30-03-16

---
+++ src/Crm/UsuariosBundle/Form/UsuarioType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Crm\UsuariosBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UsuarioType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('userName','text',array('attr' => array('class' => 'form-control')))
+            ->add('password', 'repeated', array(
+                    'type' => 'password',
+                    'invalid_message' => 'Los paswords no coinciden.',
+                    'options' => array('attr' => array('class' => 'input-xlarge','minlength'=>8)),
+                    'required' => false,
+                    'first_options'  => array('label' => 'Password'),
+                    'second_options' => array('label' => 'Repite password')))
+            ->add('personal')
+            ->add('rol')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Crm\UsuariosBundle\Entity\Usuarios'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'crm_usuariosbundle_usuarios';
+    }
+}
